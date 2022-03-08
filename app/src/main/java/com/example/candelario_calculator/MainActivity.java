6a30172f8146718cee6ddb7ff61186d2f710e9e1
BuildTools <unconfigured@null.spigotmc.org>
2022-03-09 04:53:31 +0800

    added methods, a bit of design, and compatibility with landscape mode

--- app/src/main/java/com/example/candelario_calculator/MainActivity.java
+++ app/src/main/java/com/example/candelario_calculator/MainActivity.java
@@ -13,29 +13,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText input1, input2;
     TextView ans;
     Button btnAdd, btnSub, btnMod, btnMul, btnDiv;
-
     double var1, var2;
+    String answer = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // call for buttons
         btnAdd = findViewById(R.id.btnAdd);
         btnSub = findViewById(R.id.btnSub);
         btnMod = findViewById(R.id.btnMod);
         btnMul = findViewById(R.id.btnMul);
         btnDiv = findViewById(R.id.btnDiv);
 
-        // call for edit text
         input1 = findViewById(R.id.input1);
         input2 = findViewById(R.id.input2);
 
-        // call for textView
         ans = findViewById(R.id.Ans);
 
-        // listener
         btnAdd.setOnClickListener(this);
         btnSub.setOnClickListener(this);
         btnMod.setOnClickListener(this);
@@ -51,24 +47,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId()) {
             case R.id.btnAdd:
-                ans.setText(Double.toString(var1 + var2));
-
+                ans.setText(evaluate(var1, var2, 0, answer));
                 break;
             case R.id.btnSub:
-                ans.setText(Double.toString(var1 - var2));
-
+                ans.setText(evaluate(var1, var2, 1, answer));
                 break;
             case R.id.btnMul:
-                ans.setText(Double.toString(var1 * var2));
-
+                ans.setText(evaluate(var1, var2, 2, answer));
                 break;
             case R.id.btnDiv:
-                ans.setText(Double.toString(var1 / var2));
-
+                ans.setText(evaluate(var1, var2, 3, answer));
                 break;
             case R.id.btnMod:
-                ans.setText(Double.toString(var1 % var2));
+                ans.setText(evaluate(var1, var2, 4, answer));
                 break;
         }
+    }
+
+    public String evaluate(Double var1, Double var2, int i, String Answer) {
+        switch (i){
+            case 0:
+                Answer = Double.toString(var1 + var2);
+                break;
+            case 1:
+                Answer = Double.toString(var1 - var2);
+                break;
+            case 2:
+                Answer = Double.toString(var1 * var2);
+                break;
+            case 3:
+                Answer = Double.toString(var1 / var2);
+                break;
+            case 4:
+                Answer = Double.toString(var1 % var2);
+                break;
+        }
+        return Answer;
     }
 }
